d8032e4a54d2ce9f29348c851d5ed002f5c78e1a
Daniel <mail@aritas.de>
2012-02-24 19:30:26 +0100

    add form validation

--- application/controllers/ExamsController.php
+++ application/controllers/ExamsController.php
@@ -5,198 +5,93 @@ class ExamsController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
     }
 
     public function indexAction()
     {
-        // action body
         $this->_helper->redirector('groups'); 
     }
 
     public function groupsAction()
     {
-         $form = new Application_Form_DegreeGroups();
-         
-         $groups = new Application_Model_DegreeGroupMapper();
-         $entries = $groups->fetchAll();
-         
-         $options = array();
-         
-         foreach($entries as $group)
-         {
-            $options[$group->getId()] = $group->getName();
-         }
-         
-         $form->setMultiOptions($options);
-         $this->view->form = $form;
+         $this->view->form = new Application_Form_DegreeGroups();
     }
 
     public function degreesAction()
     {
-        $degrees = new Application_Model_DegreeMapper();
-        
         $form = new Application_Form_ExamDegrees();
+        $formOrigin = new Application_Form_DegreeGroups();
     
-        $request = $this->getRequest();
-        //$formOrigin = new Application_Form_DegreeGroups();
- 
         if ($this->getRequest()->isPost()) {
-            // check if we get 2 submit parameter the group id und the submit button
-            if (count($request->getPost()) == 2) {
+            if($formOrigin->isValid($this->getRequest()->getPost())) {
             
-                $post = $request->getPost();
-
-                $entries = $degrees->fetchByGroup($post['group']);
-                
-                $options = array();
-                
-                foreach($entries as $group)
-                {
-                   $options[$group->getId()] = $group->getName();
-                }
-                $form->setMultiOptions($options);
-                
+                $post = $this->getRequest()->getPost();
+                $form->setMultiOptions($post['group']);
+                $form->setGroup($post['group']);
+                $this->view->form = $form;
                 
             } else {
-                return $this->_helper->redirector('groups');
+                $this->view->form = $formOrigin;
             }
         } else {
             // to request (direct call)
             return $this->_helper->redirector('groups');
         }
- 
-        $this->view->form = $form;
     }
 
     public function coursesAction()
     {
-        $courses = new Application_Model_CourseMapper();
-        $lecturers = new Application_Model_LecturerMapper();
-        $semesters = new Application_Model_SemesterMapper();
-        $types = new Application_Model_ExamTypeMapper();
-        
         $form = new Application_Form_ExamCourses();
-    
-        $request = $this->getRequest();
-        //$formOrigin = new Application_Form_DegreeGroups();
+        $formOrigin = new Application_Form_ExamDegrees();
  
         if ($this->getRequest()->isPost()) {
-            // check if we get 2 submit parameter the group id und the submit button
-            if (count($request->getPost()) == 2) {
+            $post = $this->getRequest()->getPost();
+        
+            //setup the origin form
+            $formOrigin->setMultiOptions($post['group']);
+            $formOrigin->setGroup($post['group']);
             
-                $post = $request->getPost();
+            if($formOrigin->isValid($this->getRequest()->getPost())) {
+                
+                $form->setCourseOptions($post['degree']);
+                $form->setLecturerOptions($post['degree']);
+                $form->setDegree($post['degree']);
+                $this->view->form = $form;
 
-                $entries = $courses->fetchByDegree($post['degree']);
-                
-                $options = array();
-                
-                foreach($entries as $group)
-                {
-                   $options[$group->getId()] = $group->getName();
-                }
-                $form->setCourseOptions($options);
-                
-                // set lecturers
-                $entries = $lecturers->fetchByDegree($post['degree']);
-                
-                $options = array();
-                
-                foreach($entries as $group)
-                {
-                   $options[$group->getId()] = $group->getName();
-                }
-                $form->setLecturerOptions($options);
-                
-                
-                // set semester
-                $entries = $semesters->fetchAll();
-                
-                $options = array();
-                
-                foreach($entries as $group)
-                {
-                   $options[$group->getId()] = $group->getName();
-                }
-                $form->setSemesterOptions($options);
-                
-                
-                // set types
-                $entries = $types->fetchAll();
-                
-                $options = array();
-                
-                foreach($entries as $group)
-                {
-                   $options[$group->getId()] = $group->getName();
-                }
-                $form->setExamTypeOptions($options);
-                
-                
-                
             } else {
-                // wrong request (e.g. not the right number of variables)
-                return $this->_helper->redirector('degrees');
+                $this->view->form = $formOrigin;
             }
         } else {
             // no request
             return $this->_helper->redirector('degrees');
         }
- 
-        $this->view->form = $form;
     }
 
     public function searchAction()
     {
-        /*  Example Parameters
-            array (
-              'controller' => 'exams',
-              'action' => 'search',
-              'module' => 'default',
-              'course' => 
-              array (
-                0 => '1',
-              ),
-              'lecturer' => 
-              array (
-                0 => '1',
-              ),
-              'semester' => 
-              array (
-                0 => '62',
-              ),
-              'examType' => 
-              array (
-                0 => '-1',
-              ),
-              'submit' => 'Weiter',
-            )
-        */
-        $exams = new Application_Model_ExamMapper();
+        $formOrigin = new Application_Form_ExamCourses();
         
         $request = $this->getRequest();
         $this->view->exams = array();
         
         if ($this->getRequest()->isPost()) {
-            // check if we get 2 submit parameter the group id und the submit button
-            if (count($request->getPost()) == 5) {
+            $post = $request->getPost();
+            $formOrigin->setCourseOptions($post['degree']);
+            $formOrigin->setLecturerOptions($post['degree']);
+            $formOrigin->setDegree($post['degree']);
+            
+            if($formOrigin->isValid($this->getRequest()->getPost())) {
                 
                 $data = $request->getPost();
-                
-                //var_dump($data);
-                
-                $this->view->exams = $exams->fetch($data['course'],$data['lecturer'],$data['semester'],$data['examType']);
+
+                $exams = new Application_Model_ExamMapper();
+                $this->view->exams = $exams->fetch($data['course'],$data['lecturer'],$data['semester'],$data['examType'], $data['degree']);
                 
             } else {
-                // ToDo: redcrict will fail, no parameter for courses
-                return $this->_helper->redirector('courses');
+                $this->view->form = $formOrigin;
             }
-            
         }
-        
     }
-
-
 }
 
 
--- application/forms/DegreeGroups.php
+++ application/forms/DegreeGroups.php
@@ -6,13 +6,15 @@ class Application_Form_DegreeGroups extends Zend_Form
 
     public function init()
     {
-       // Set the method for the display form to POST
         $this->setMethod('post');
+        $this->setAction('/exams/degrees');
  
         $this->_elementSelect = new Zend_Form_Element_Select('group');
         
         $this->_elementSelect->setAttrib('size', '3');
-        $this->_elementSelect->addValidator('NotEmpty', false);
+        $this->_elementSelect->setRequired(true);
+        
+        $this->setMultiOptions();
         
         $this->addElement($this->_elementSelect);
         
@@ -23,9 +25,19 @@ class Application_Form_DegreeGroups extends Zend_Form
         ));
     }
     
-    public function setMultiOptions(array $options)
+    public function setMultiOptions()
     {
-        $this->_elementSelect->setMultiOptions($options);
+        $groups = new Application_Model_DegreeGroupMapper();
+        $entries = $groups->fetchAll();
+         
+        $options = array();
+         
+        foreach($entries as $group)
+        {
+            $options[$group->getId()] = $group->getName();
+        }
+
+         $this->_elementSelect->setMultiOptions($options);
     }
 
 }
