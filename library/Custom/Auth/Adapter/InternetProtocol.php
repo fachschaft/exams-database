1398906466e1581a1c738101be672edb38e423fe
William Glover <williamglover@web.de>
2012-04-12 23:26:24 +0200

    Make clearer what happerns when authing IPs, make nice later

--- library/Custom/Auth/Adapter/InternetProtocol.php
+++ library/Custom/Auth/Adapter/InternetProtocol.php
@@ -15,10 +15,11 @@ class Custom_Auth_Adapter_InternetProtocol implements Zend_Auth_Adapter_Interfac
 	public function authenticate()
 	{
 		if(preg_match($this->_regExp, $this->_Ip) == 0) {
-			return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID,$this->_username);
+			return new Zend_Auth_Result(Zend_Auth_Result::FAILURE_CREDENTIAL_INVALID,NULL);
 		}
-		
-		return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS,$this->_username);
+		// TODO Make this nicer by giving ips a session and checking for an admin identity in the admin controller
+		// NULL so that success is returned, but no session is created so that ips can't access the admin controller
+		return new Zend_Auth_Result(Zend_Auth_Result::SUCCESS,NULL);
 	}
 	
 }
