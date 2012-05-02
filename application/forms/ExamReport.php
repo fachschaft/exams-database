b9c1fa80f8d55ed14075b6cef8aad43bfd5326ca
aritas1 <mail@aritas.de>
2012-05-03 01:59:41 +0200

    add PHPDoc to files, fix the header part of #4

--- application/forms/ExamReport.php
+++ application/forms/ExamReport.php
@@ -1,4 +1,14 @@
-<?php
+<?php 
+ /**
+ * exams-database
+ * @copyright   Written for Fachschaft Technische Fakultät Freiburg, Germany and licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * @link		https://github.com/aritas1/exams-database/
+ * @author		Daniel Leinfelder <mail@aritas.de>
+ * @author		William Glover <william@aamu-uninen.de>
+ * @version		1.0
+ * @since		1.0
+ * @todo        -
+ */
 
 class Application_Form_ExamReport extends Zend_Form {
 	
