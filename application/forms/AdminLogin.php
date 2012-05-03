f945c05e046f897c38d7c2ed4e56008e64db9876
aritas1 <mail@aritas.de>
2012-05-03 02:14:22 +0200

    fix spacing #4

--- application/forms/AdminLogin.php
+++ application/forms/AdminLogin.php
@@ -1,13 +1,13 @@
 <?php 
- /**
+/**
  * exams-database
- * @copyright   Written for Fachschaft Technische Fakultät Freiburg, Germany and licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * @copyright	Written for Fachschaft Technische Fakultät Freiburg, Germany and licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
  * @link		https://github.com/aritas1/exams-database/
  * @author		Daniel Leinfelder <mail@aritas.de>
  * @author		William Glover <william@aamu-uninen.de>
  * @version		1.0
  * @since		1.0
- * @todo        -
+ * @todo		-
  */
 
 class Application_Form_AdminLogin extends Zend_Form
