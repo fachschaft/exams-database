cb01c3ffeef53cf214d63fb4dd927f9c6f2e2880
aritas1 <mail@aritas.de>
2012-08-15 13:05:27 +0200

    fix ip login error

--- application/models/UserPrivilegeMappingMapper.php
+++ application/models/UserPrivilegeMappingMapper.php
@@ -27,10 +27,46 @@ class Application_Model_UserPrivilegeMappingMapper
 	
 	public function getRole($adapter, $identity = null)
 	{
+		
 		if($identity == null) {
 			$identity = "";
 		} else {
 			$identity = ' AND identity = "'.$identity.'"';
+		}
+		
+		if($adapter == 'Custom_Auth_Adapter_InternetProtocol') {
+			$res = $this->getDbTable()->fetchAll('authadapter = "'.$adapter.'" '.$identity);
+			
+			if(count($res) == 0) {
+				// no special role set for this ip
+				// select default role
+				$res = $this->getDbTable()->fetchAll('authadapter = "'.$adapter.'" AND identity IS NULL');
+				
+				if(count($res) == 1) {
+					$role = Application_Model_AuthManager::mapRolteToOrder($res[0]['role']);
+					return $role['string'];
+				} elseif(count($res) >= 1) {
+					// more than one default role?
+					$role = Application_Model_AuthManager::$Guest;
+				} else {
+					$role = Application_Model_AuthManager::$Guest;
+				}
+				return $role['string'];
+			} else {
+				// order
+			
+				$role = Application_Model_AuthManager::$Guest;
+
+				foreach ($res as $priv) {
+					$or = Application_Model_AuthManager::mapRolteToOrder($priv['role']);
+					if($role['order'] < $or['order'])
+					{
+						$role = $or;
+					}
+				return $role['string'];
+				}
+			}
+			
 		}
 		
 		$res = $this->getDbTable()->fetchAll('authadapter = "'.$adapter.'" '.$identity);
