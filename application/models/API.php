5e5b25824caffbaa177656ace196e0a70ce6bbd0
aritas1 <mail@aritas.de>
2012-10-16 18:25:20 +0200

    change api auth to master key table

--- application/models/API.php
+++ application/models/API.php
@@ -1,7 +1,40 @@
 <?php
+/**
+ * exams-database
+ * @copyright	Written for Fachschaft Technische Fakultaet Freiburg, Germany and licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * @link		https://github.com/aritas1/exams-database/
+ * @author		Daniel Leinfelder <mail@aritas.de>
+ * @author		William Glover <william@aamu-uninen.de>
+ * @version		1.0
+ * @since		1.0
+ * @todo		-
+ */
 
 class Application_Model_API
 {	
+	protected $_dbTable;
+	
+	public function setDbTable($dbTable)
+	{
+		if (is_string($dbTable)) {
+			$dbTable = new $dbTable();
+		}
+		if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+			throw new Exception('Invalid table data gateway provided');
+		}
+		$this->_dbTable = $dbTable;
+		return $this;
+	}
+	
+	public function getDbTable()
+	{
+		if (null === $this->_dbTable) {
+			$this->setDbTable('Application_Model_DbTable_APIMasterKey');
+		}
+		return $this->_dbTable;
+	}
+	
+	
 	public function uncheckedExams($apiKey)
 	{
 		// check if api key ist valid
@@ -17,7 +50,14 @@ class Application_Model_API
 	
 	private function validateKey($apiKey)
 	{
-		return true;
+		$result = $this->getDbTable()->getAdapter()->query("SELECT count(*) as count FROM  `api_master_key` WHERE  `key` LIKE  '".$apiKey."'");
+		foreach ($result as $res) {
+			if($res['count'] == 1)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
---
+++ application/models/DbTable/APIMasterKey.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * exams-database
+ * @copyright	Written for Fachschaft Technische Fakultaet Freiburg, Germany and licensed under a Creative Commons Attribution-ShareAlike 3.0 Unported License.
+ * @link		https://github.com/aritas1/exams-database/
+ * @author		Daniel Leinfelder <mail@aritas.de>
+ * @author		William Glover <william@aamu-uninen.de>
+ * @version		1.0
+ * @since		1.0
+ * @todo		-
+ */
+
+class Application_Model_DbTable_APIMasterKey extends Zend_Db_Table_Abstract
+{
+
+    protected $_name = 'api_master_key';
+    protected $_primary = 'idapi_master_key';
+
+
+}
+
